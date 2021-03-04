bf71124f30edda98adbe4cbc9387b9a4aa5a1caf
t-rolfin <rolfin.dc@gmail.com>
2021-03-04 18:23:54 +0200

    Added new MetaResponse. ( Forbidden )

---
+++ src/Rolfin.Result/MetaResponses/Forbidden.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rolfin.Result.MetaResponses
+{
+    public class Forbidden : IMetaResponse
+    {
+        public int Code => 403;
+        public string Name => "Forbidden";
+        public string Message => "You are not allowed to access this information.";
+    }
+}
