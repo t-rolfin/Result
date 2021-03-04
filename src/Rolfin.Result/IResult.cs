44f879267050bd45fd40dce6fb9eb6b3edc2c574
t-rolfin <rolfin.dc@gmail.com>
2021-03-04 19:16:27 +0200

    Updated 'IResult' interface.

--- src/Rolfin.Result/IResult.cs
+++ src/Rolfin.Result/IResult.cs
@@ -1,8 +1,15 @@
-﻿namespace Rolfin.Result
+﻿using System;
+
+namespace Rolfin.Result
 {
     public interface IResult<T>
     {
+        Type GetValueType { get; }
         bool IsSuccess { get; }
+
+        [Obsolete]
+        string Message { get; }
+        IMetaResult MetaResult { get; }
         T Value { get; }
     }
 }
