7b66a911abc649261d6cb35f331ceb7a7a10e537
t-rolfin <rolfin.dc@gmail.com>
2021-03-07 09:53:20 +0200

    Changed structure of the 'Result'. This will not affect your code if you will update to this version.

---
+++ src/Rolfin.Result/BaseResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rolfin.Result
+{
+    public abstract class BaseResult<T>
+    {
+        private bool isSuccess;
+        public bool IsSuccess
+        {
+            get { return IsSuccess; }
+            protected set { isSuccess = value; }
+        }
+
+
+        public IMetaResult MetaResult { get; protected set; }
+
+
+        public Type GetValueType
+            => typeof(T);
+
+        /// <summary>
+        /// This helps you to customize your MetaResponse 
+        /// with your own massage, code, and name response
+        /// </summary>
+        /// <typeparam name="CType"> Type of custom MetaResponse </typeparam>
+        /// <param name="metaResponse"> Insance of your custom MetaResponse </param>
+        /// <returns> Result<T> with you custom MetaResponse </returns>
+        public virtual BaseResult<T> With<CType>()
+            where CType : IMetaResult
+        {
+            this.MetaResult = Activator.CreateInstance<CType>();
+            return this;
+        }
+    }
+}
--- src/Rolfin.Result/IResult.cs
+++ src/Rolfin.Result/IResult.cs
@@ -4,10 +4,6 @@ namespace Rolfin.Result
 {
     public interface IResult<T>
     {
-        Type GetValueType { get; }
-        bool IsSuccess { get; }
-
-        IMetaResult MetaResult { get; }
         T Value { get; }
     }
 }
