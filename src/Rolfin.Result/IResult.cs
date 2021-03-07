e2bb3e7a5666e24e64dd304033f343a8ed353eee
Florin Tatu <70466418+t-rolfin@users.noreply.github.com>
2021-03-07 12:30:41 +0200

    Result (#8)
    * Removed 'Message' property from 'Result' and 'IResult'.
    
    * Changed structure of the 'Result'. This will not affect your code if you will update to this version.
    
    * Solved the bug with protection level for isSuccess field.
    
    * Added constructors for 'Result', implementation of 'Result<string>'.
    
    * Initialized the value of 'MetaResult' with Ok result when an result is created.
    
    * Initialized 'MetaResult'  with 'NotFound' into 'Invalid' factory calls.

--- src/Rolfin.Result/IResult.cs
+++ src/Rolfin.Result/IResult.cs
@@ -4,12 +4,6 @@ namespace Rolfin.Result
 {
     public interface IResult<T>
     {
-        Type GetValueType { get; }
-        bool IsSuccess { get; }
-
-        [Obsolete]
-        string Message { get; }
-        IMetaResult MetaResult { get; }
         T Value { get; }
     }
 }
--- src/Rolfin.Result/Result.cs
+++ src/Rolfin.Result/Result.cs
@@ -7,73 +7,52 @@ namespace Rolfin.Result
 {
 
     public class Result : Result<string>
-    { }
-
-    public class Result<T> : IResult<T>
     {
         public Result() { }
+
+        public Result(string result)
+        : base(result) { }
+    }
+
+    public class Result<T> : BaseResult<T>, IResult<T>
+    {
+        public Result() { }
+
         public Result(T result)
         {
             this.Value = result;
         }
 
-        private bool isSuccess;
-
 
         public T Value { get; protected set; }
-
-        [Obsolete("You can use 'MetaResult' to customize you messages.")]
-        public string Message { get; protected set; }
-
-        public IMetaResult MetaResult { get; protected set; }
-
-
-        public Type GetValueType
-            => typeof(T);
-
-        public bool IsSuccess
-            => isSuccess;
-
-        /// <summary>
-        /// This helps you to customize your MetaResponse 
-        /// with your own massage, code, and name response
-        /// </summary>
-        /// <typeparam name="CType"> Type of custom MetaResponse </typeparam>
-        /// <param name="metaResponse"> Insance of your custom MetaResponse </param>
-        /// <returns> Result<T> with you custom MetaResponse </returns>
-        public Result<T> With<CType>()
-            where CType : IMetaResult
-        {
-            this.MetaResult = Activator.CreateInstance<CType>(); ;
-            return this;
-        }
 
 
         public static Result<T> Success()
         {
             return new Result<T>()
             {
-                isSuccess = true,
+                IsSuccess = true,
                 Value = default(T)
             };
         }
 
         public static Result<T> Success(T result)
         {
-            return new Result<T>(result) { isSuccess = true };
+            return new Result<T>(result) { IsSuccess = true };
         }
 
         public static Result<R> Success<R>(R result)
         {
-            return new Result<R>(result) { isSuccess = true };
+            return new Result<R>(result) { IsSuccess = true };
         }
 
         public static Result<T> Invalid()
         {
             return new Result<T>()
             {
-                isSuccess = false,
-                Value = default(T)
+                IsSuccess = false,
+                Value = default(T),
+                MetaResult = new NotFound()
             };
         }
 
@@ -81,15 +60,18 @@ namespace Rolfin.Result
         {
             return new Result<T>()
             {
-                isSuccess = false,
-                Message = message,
+                IsSuccess = false,
                 MetaResult = new Custom { Message = message }
             };
         }
 
         public static Result<R> Invalid<R>(R result)
         {
-            return new Result<R>(result) { isSuccess = false };
+            return new Result<R>(result) 
+            { 
+                IsSuccess = false,
+                MetaResult = new NotFound()
+            };
         }
 
     }
