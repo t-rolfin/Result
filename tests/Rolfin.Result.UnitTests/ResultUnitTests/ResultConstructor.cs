fccbe847f9bdc8b3c72018848d06706f376bc398
t-rolfin <rolfin.dc@gmail.com>
2021-03-07 13:00:45 +0200

    Added tests for Result and Result<T>.

---
+++ tests/Rolfin.Result.UnitTests/ResultUnitTests/ResultConstructor.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Rolfin.Result;
+using Rolfin.Result.MetaResults;
+using Rolfin.Result.UnitTests.Resources;
+
+namespace Rolfin.Result.UnitTests.ResultUnitTests
+{
+    public class ResultConstructor
+    {
+        [Fact]
+        public void Result_ShouldReturnStringMessage_WhenProvideStringValueIntoConstructor()
+        {
+            string expectedResult = "Test message.";
+
+            //Result is an inplementation of Result<string>
+            var result = new Result(expectedResult);
+
+            Assert.Equal(expectedResult, result.Value);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnStringMessage_WhenProvideStringValueIntoConstructor()
+        {
+            
+            string expectedResult = "Other test message.";
+
+            var result = new Result<string>(expectedResult);
+
+            Assert.Equal(expectedResult, result.Value);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnObject_WhenSetGenericTypeAsObject()
+        {
+            object expectedObject = new object();
+
+            var result = new Result<object>(expectedObject);
+
+            Assert.Equal(expectedObject, result.Value);
+        }
+
+        [Fact]
+        public void GeneticResult_ShouldReturnNull_WhenPassNullValue()
+        {
+            var result = new Result<object>(null);
+
+            Assert.Null(result.Value);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(123)]
+        [InlineData("test value")]
+        public void GenericResult_ShouldReturnOk_WhenGiveAValue(object value)
+        {
+            var result = new Result<object>(value);
+
+            Assert.Equal(new Ok().Code, result.MetaResult.Code);
+            Assert.Equal(value, result.Value);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldCustomizeMetaResult_WhenGiveDefaultCustomMetaResult()
+        {
+            NotFound expectedResult = new NotFound();
+
+            var result = new Result<object>().With<NotFound>();
+
+            Assert.Equal(expectedResult.Code, result.MetaResult.Code);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnNotFound_WhenInvalidFactoryCall()
+        {
+            NotFound expectedResult = new NotFound();
+
+            var result = Result<object>.Invalid();
+
+            Assert.Equal(expectedResult.Code, result.MetaResult.Code);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnCustomMetaResult_WhenGiveMyCustomMetaResult()
+        {
+            CustomMetaResult expectedMetaResult = new CustomMetaResult();
+
+            var result = Result<object>.Invalid()
+                .With<CustomMetaResult>();
+
+            Assert.Equal(expectedMetaResult.Code, result.MetaResult.Code);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnCustomInvaldResult_WhenCallInvalidWithMessage()
+        {
+            string expectedMessage = "Custom invalid message.";
+
+            var result = Result<object>.Invalid(expectedMessage);
+
+            Assert.Equal(expectedMessage, result.MetaResult.Message);
+            Assert.Equal(0, result.MetaResult.Code);
+        }
+
+        [Fact]
+        public void Result_ShouldReturnInvalidCustomResultType_WhenCallInvalidWhitCustomType()
+        {
+            NotFound expectedMetaResult = new NotFound();
+
+            var result = Result.Invalid<CustomMetaResult>(new CustomMetaResult());
+
+            Assert.Equal(typeof(CustomMetaResult), result.GetValueType);
+            Assert.Equal(expectedMetaResult.Code, result.MetaResult.Code);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnOkMetaData_WhenSuccessFactoryCall()
+        {
+            Ok expectedResult = new Ok();
+
+            var result = Result<object>.Success();
+
+            Assert.Equal(expectedResult.Code, result.MetaResult.Code);
+        }
+
+        [Fact]
+        public void GenericResult_ShouldReturnOkWithCustomValue_WhenCallSuccessWithParameter()
+        {
+            Ok expectedResult = new Ok();
+            object expectedObject = new object();
+
+            var result = Result<object>.Success(expectedObject);
+
+            Assert.Equal(expectedResult.Code, result.MetaResult.Code);
+            Assert.Equal(expectedObject, result.Value);
+        }
+
+        [Fact]
+        public void Result_ShouldReturnSuccessCustomResultType_WhenCallSuccessWhitCustomType()
+        {
+            Ok expectedMetaResult = new Ok();
+
+            var result = Result.Success(new CustomMetaResult());
+
+            Assert.Equal(typeof(CustomMetaResult), result.GetValueType);
+            Assert.Equal(expectedMetaResult.Code, result.MetaResult.Code);
+        }
+    }
+}
